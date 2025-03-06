80ff269995594ac4d00223b44cc3b168162aa89d
AdityaRaj00007 <rajgupta49102@gmail.com>
2025-03-06 14:46:44 +0530

    [Aditya] Add . UC-4 Implemented two-way data binding for user input in AppComponent

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -1,25 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { log } from 'console';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Hello Word';
+  title = 'Hello World';
   imgUrl = "../assets/logo.jpg";
   url = "https://www.bridgelabz.com";
+  userName: string = "Aditya";
 
-ngOnInit(): void{
-  this.title = "Hello from BridgeLabz"
+  ngOnInit(): void {
+    this.title = "Hello from BridgeLabz";
   }
-  onClick(event: Event): void{
+
+  onClick(event: Event): void {
     console.log('BridgeLabz logo clicked!', event);
     window.open(this.url, '_blank'); 
-    
   }
 }
